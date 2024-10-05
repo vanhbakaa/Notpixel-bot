8630c9e8dca62c82cd646b9d3ec4014a90de9ae8
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-05 16:22:12 +0700

    Added sleep between each round

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -387,6 +387,6 @@ async def run_tapper1(tg_clients: list[Client], proxies):
             logger.info(f"Sleep {sleep_}s...")
             await asyncio.sleep(sleep_)
 
-        sleep_ = randint(520, 700)
+        sleep_ = randint(settings.SLEEP_TIME_BETWEEN_EACH_ROUND[0], settings.SLEEP_TIME_BETWEEN_EACH_ROUND[1])
         logger.info(f"<red>Sleep {sleep_}s...</red>")
         await asyncio.sleep(sleep_)
