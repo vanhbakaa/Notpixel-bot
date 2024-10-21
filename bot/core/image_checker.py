c94994eee93dee75f68465bf26e1bbcb6be38028
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-21 20:46:14 +0700

    Update image_checker.py

--- bot/core/image_checker.py
+++ bot/core/image_checker.py
@@ -5,7 +5,7 @@ import time
 from bot.utils import logger
 
 
-ENDPOINT = "https://www.notpxapi.xyz"
+ENDPOINT = "https://notpxapi.xyz"
 def reachable(times_to_fall=20):
     try:
         response = requests.get(f"{ENDPOINT}/is_reacheble/", verify=False)
