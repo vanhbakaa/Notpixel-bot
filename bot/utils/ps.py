a2496bb7b16c59b626e06fd93e2193c440df4c6d
Vanhbaka <vanhbaka2k7@gmail.com>
2024-10-22 15:00:52 +0700

    Update ps.py

--- bot/utils/ps.py
+++ bot/utils/ps.py
@@ -1,8 +1,31 @@
 import requests
 import re
+
+from bot.config import settings
 from bot.utils import logger
 
 baseUrl = "https://notpx.app/api/v1/"
+
+apis = [
+    "/users/me",
+    "/users/stats",
+    "/image/template/my",
+    "/mining/status",
+    "/image/template/subscribe/",
+    "/mining/claim",
+    "/mining/boost/check/",
+    "/mining/task/check/"
+]
+ls_pattern = re.compile(r'ls\s*=\s*["\'](https?://[^\s"\']+)["\']')
+e_get_pattern = re.compile(r'E\.get\(\s*["\']([^"\']+)["\']|E\.get\(\s*`([^`]+)`\s*\)')
+e_put_pattern = re.compile(r'E\.put\(\s*["\']([^"\']+)["\']|E\.put\(\s*`([^`]+)`\s*\)')
+
+
+def clean_url(url):
+    url = url.split('?')[0]
+    url = re.sub(r'\$\{.*?\}', '', url)
+    url = re.sub(r'//+', '/', url)
+    return url
 
 def get_main_js_format(base_url):
     try:
@@ -25,13 +48,28 @@ def get_base_api(url):
         response = requests.get(url)
         response.raise_for_status()
         content = response.text
-        match = re.findall( r'=\s*"(https?://[^"]+)"', content)
+        match = ls_pattern.search(content)
+        e_get_urls = e_get_pattern.findall(content)
+        e_put_urls = e_put_pattern.findall(content)
+
+        if e_get_urls is None:
+            return None
+
+        urls = [url[0] if url[0] else url[1] for url in e_get_urls]
+        urls_put = [url[0] if url[0] else url[1] for url in e_put_urls]
+        clean_urls = [clean_url(url) for url in urls] + [clean_url(url) for url in urls_put]
+
+        for url in apis:
+            if url not in clean_urls:
+                logger.warning(f"<yellow>api {url} changed!</yellow>")
+                return None
 
         if match:
-            return match
+            return match.group(1)
         else:
             logger.info("Could not find 'api' in the content.")
             return None
+
     except requests.RequestException as e:
         logger.warning(f"Error fetching the JS file: {e}")
         return None
@@ -42,18 +80,30 @@ def check_base_url():
     main_js_formats = get_main_js_format(base_url)
 
     if main_js_formats:
-        for format in main_js_formats:
-            logger.info(f"Trying format: {format}")
-            full_url = f"https://app.notpx.app{format}"
-            result = get_base_api(full_url)
-            # print(f"{result} | {baseUrl}")
-            if baseUrl in result:
-                logger.success("<green>No change in api!</green>")
-                return True
+        if settings.AVANCED_ANTI_DETECTION:
+            r = requests.get("https://raw.githubusercontent.com/vanhbakaa/Notpixel-bot/refs/heads/main/cgi")
+            js_ver = r.text.strip()
+            for js in main_js_formats:
+                if js_ver in js:
+                    logger.success(f"<green>No change in js file: {js_ver}</green>")
+                    return True
             return False
         else:
-            logger.warning("Could not find 'baseURL' in any of the JS files.")
-            return False
+            for format in main_js_formats:
+                logger.info(f"Trying format: {format}")
+                full_url = f"https://app.notpx.app{format}"
+                result = get_base_api(full_url)
+                # print(f"{result} | {baseUrl}")
+                if result is None:
+                    return False
+
+                if baseUrl in result:
+                    logger.success("<green>No change in api!</green>")
+                    return True
+                return False
+            else:
+                logger.warning("Could not find 'baseURL' in any of the JS files.")
+                return False
     else:
         logger.info("Could not find any main.js format. Dumping page content for inspection:")
         try:
