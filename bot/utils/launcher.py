8e51465c027cb56525a5ee8601adc4357ba71c7b
Vanhbaka <vanhbaka2k7@gmail.com>
2024-11-10 03:10:06 +0700

    Save and load ua from file

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -1,19 +1,22 @@
+import json
 import os
 import glob
 import asyncio
 import argparse
 import sys
 from itertools import cycle
+from urllib.parse import unquote
 
+from aiofile import AIOFile
 from pyrogram import Client
 from better_proxy import Proxy
 
 from bot.config import settings
+from bot.core.agents import generate_random_user_agent
 from bot.utils import logger
 from bot.core.tapper import run_tapper, run_tapper1
 from bot.core.query import run_query_tapper, run_query_tapper1
 from bot.core.registrator import register_sessions
-from .ps import check_base_url
 
 
 start_text = """
@@ -26,6 +29,8 @@ Select an action:
 """
 
 global tg_clients
+
+
 
 def get_session_names() -> list[str]:
     session_names = sorted(glob.glob("sessions/*.session"))
@@ -70,6 +75,32 @@ async def get_tg_clients() -> list[Client]:
 
     return tg_clients
 
+def fetch_username(query):
+    try:
+        fetch_data = unquote(query).split("&user=")[1].split("&auth_date=")[0]
+        json_data = json.loads(fetch_data)
+        return json_data['username']
+    except:
+        logger.warning(f"Invaild query: {query}")
+        sys.exit()
+
+
+async def get_user_agent(session_name):
+    async with AIOFile('user_agents.json', 'r') as file:
+        content = await file.read()
+        user_agents = json.loads(content)
+
+    if session_name not in list(user_agents.keys()):
+        logger.info(f"{session_name} | Doesn't have user agent, Creating...")
+        ua = generate_random_user_agent(device_type='android', browser_type='chrome')
+        user_agents.update({session_name: ua})
+        async with AIOFile('user_agents.json', 'w') as file:
+            content = json.dumps(user_agents, indent=4)
+            await file.write(content)
+        return ua
+    else:
+        logger.info(f"{session_name} | Loading user agent from cache...")
+        return user_agents[session_name]
 
 async def process() -> None:
     parser = argparse.ArgumentParser()
@@ -79,6 +110,11 @@ async def process() -> None:
     action = parser.parse_args().action
     ans = parser.parse_args().multithread
     logger.info(f"Detected {len(get_session_names())} sessions | {len(get_proxies())} proxies")
+
+    if not os.path.exists("user_agents.json"):
+        with open("user_agents.json", 'w') as file:
+            file.write("{}")
+        logger.info("User agents file created successfully")
 
     if not action:
         print(start_text)
@@ -136,14 +172,12 @@ async def process() -> None:
 async def run_tasks_query(query_ids: list[str]):
     proxies = get_proxies()
     proxies_cycle = cycle(proxies) if proxies else None
-    account_name = [i for i in range(len(query_ids) + 10)]
-    name_cycle = cycle(account_name)
     tasks = [
         asyncio.create_task(
             run_query_tapper(
                 query=query,
                 proxy=next(proxies_cycle) if proxies_cycle else None,
-                name=f"Account{next(name_cycle)}"
+                ua=await get_user_agent(fetch_username(query))
             )
         )
         for query in query_ids
@@ -158,6 +192,7 @@ async def run_tasks(tg_clients: list[Client]):
             run_tapper(
                 tg_client=tg_client,
                 proxy=next(proxies_cycle) if proxies_cycle else None,
+                ua=await get_user_agent(tg_client.name)
             )
         )
         for tg_client in tg_clients
